567df2b8f6be81311c670779b4542e90784d5bff
Sergio Mendez <sergioarm.gpl@gmail.com>
2022-07-13 01:03:22 -0600

    feat: changes yaml

--- ch14/python/object_detection/run.sh
+++ ch14/python/object_detection/run.sh
@@ -1,10 +1,16 @@
 echo "killing previous port-forward"
+#P=$(ps -ax | grep detect | grep -v grep | awk '{ print $1 }');kill $P
+killall chromium-browser
+killall lxterminal
 P=$(ps -ax | grep kubectl | grep -v grep | awk '{ print $1 }');for p in ${P[*]}; do kill $p; done
-sleep 5
+sleep 15
 echo "starting new port-forwards"
 screen -dm bash -c 'kubectl port-forward --address 0.0.0.0 deploy/inference 3000:3000'
 screen -dm bash -c 'kubectl port-forward --address 0.0.0.0 deploy/gps-queue 3001:3000'
+screen -dm bash -c 'kubectl port-forward --address 0.0.0.0 deploy/proxy 5001:5000'
+screen -dm bash -c 'kubectl port-forward --address 0.0.0.0 deploy/traffic-map 5000:3000'
 sleep 10
 echo "loading object detection"
 PREDICT_HOST="http://localhost:3000" GPS_QUEUE_HOST="http://localhost:3001" python3 detect.py --model efficientdet_lite0.tflite --cameraId 0 #--enableEdgeTPU
-sleep 10
+echo "Program finalized"
+sleep 4
