f9db72926f1c801738f1ee14a99310da6e1015b9
Sergio Méndez <sergio.mendez@yalo.com>
2022-09-29 01:39:30 -0600

    chore: changed install_shortcuts.sh

--- ch14/code/python/object_detection/install_shortcuts.sh
+++ ch14/code/python/object_detection/install_shortcuts.sh
@@ -1,1 +1,12 @@
-cp *.desktop /home/$USER/Desktop
+cp traffic.desktop /home/$USER/Desktop
+#cp *.desktop /home/$USER/Desktop
+cat <<EOF > /home/$USER/Desktop
+[Desktop Entry]
+Comment=Detector
+Terminal=true
+Name=Detector
+Exec=/bin/bash /home/$USER/Edge-computing-with-K3os-and-k3s/ch14/code/python/object_detection/run.sh
+Type=Application
+Icon=/usr/share/pixmaps/chromium-browser.png
+Path=/home/$USER/Edge-computing-with-K3os-and-k3s/ch14/code/python/object_detection
+EOF
